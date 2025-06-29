a6c91b92783e26808a092b31e726c656b89f8bcf
wilfredtinega <tinegawilfred2@gmail.com>
2025-06-29 19:04:51 +0300

    Day One bootcamp assignment

---
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Wilfred Tinega and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class AirplaneTicket(Document):
+	pass
---
+++ airplane_mode/airplane_mode/doctype/airport/airport.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Wilfred Tinega and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Airport", {
+// 	refresh(frm) {
+
+// 	},
+// });
